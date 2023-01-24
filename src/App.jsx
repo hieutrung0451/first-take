13ebd76e0a6f14133ca39f930c6e083bd070db2c
trunghieu <trunghieu0451@gmail.com>
2023-01-25 05:47:12 +0700

    add fork on github

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,24 @@ import Gallery from "./components/Gallery";
 
 const App = () => {
   return (
-    <div className="bg-slate-200 w-full h-full container px-32 py-32 flex flex-col items-center">
-      <div>
-        <h1 className="text-5xl text-center font-bold">First Take</h1>
-        <SearchForm />
-        <MainNav />
-      </div>
-      <div>
-        <h2 className="text-3xl text-center font-bold mb-12">
-          Mountain Pictures
-        </h2>
-        <Gallery />
+    <div>
+      <span id="forkongithub">
+        <a href="https://github.com/hieutrung0451/first-take">
+          Fork me on GitHub
+        </a>
+      </span>
+      <div className="bg-slate-200 w-full h-full container px-32 py-32 flex flex-col items-center">
+        <div>
+          <h1 className="text-5xl text-center font-bold">First Take</h1>
+          <SearchForm />
+          <MainNav />
+        </div>
+        <div>
+          <h2 className="text-3xl text-center font-bold mb-12">
+            Mountain Pictures
+          </h2>
+          <Gallery />
+        </div>
       </div>
     </div>
   );
