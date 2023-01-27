466770dbe83dcd5463fa2549938310b3a99be9b1
trunghieu <trunghieu0451@gmail.com>
2023-01-27 20:39:40 +0700

    setup route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SearchForm from "./components/SearchForm";
 import MainNav from "./components/MainNav";
 import Gallery from "./components/Gallery";
@@ -8,24 +9,33 @@ import { FlickrProvider } from "./context/FlickrContext";
 const App = () => {
   return (
     <FlickrProvider>
-      <span id="forkongithub">
-        <a href="https://github.com/hieutrung0451/first-take">
-          Fork me on GitHub
-        </a>
-      </span>
-      <div className="bg-slate-200 w-full h-full container px-32 py-32 flex flex-col items-center">
-        <div>
-          <h1 className="text-5xl text-center font-bold">First Take</h1>
-          <SearchForm />
-          <MainNav />
+      <Router>
+        <span id="forkongithub">
+          <a href="https://github.com/hieutrung0451/first-take">
+            Fork me on GitHub
+          </a>
+        </span>
+        <div className="bg-slate-200 w-full h-full container px-32 py-32 flex flex-col items-center">
+          <div>
+            <h1 className="text-5xl text-center font-bold">First Take</h1>
+            <SearchForm />
+            <MainNav />
+          </div>
+          <div>
+            <h2 className="text-3xl text-center font-bold mb-12">
+              Mountain Pictures
+              <Routes>
+                <Route path="/" />
+                <Route path="/mountain" />
+                <Route path="/beaches" />
+                <Route path="/birds" />
+                <Route path="/food" />
+              </Routes>
+            </h2>
+            <Gallery />
+          </div>
         </div>
-        <div>
-          <h2 className="text-3xl text-center font-bold mb-12">
-            Mountain Pictures
-          </h2>
-          <Gallery />
-        </div>
-      </div>
+      </Router>
     </FlickrProvider>
   );
 };
--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,20 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const MainNav = () => {
   return (
     <nav className="my-14 mx-6">
       <ul className="flex w-full justify-center">
         <li className="px-4 py-2 mr-10 bg-slate-900 text-slate-200 rounded-md font-semibold">
-          Mountain
+          <Link to="/mountain">Mountain</Link>
         </li>
         <li className="px-4 py-2 mr-10 bg-slate-900 text-slate-200 rounded-md font-semibold">
           Beaches
+          <Link to="/beaches">Beaches</Link>
         </li>
         <li className="px-4 py-2 mr-10 bg-slate-900 text-slate-200 rounded-md font-semibold">
-          Birds
+          <Link to="/birds">Birds</Link>
         </li>
         <li className="px-4 py-2 mr-10 bg-slate-900 text-slate-200 rounded-md font-semibold">
-          Food
+          <Link to="/food">Food</Link>
         </li>
       </ul>
     </nav>
